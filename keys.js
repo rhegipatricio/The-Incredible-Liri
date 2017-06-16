19c5421ee4706896d03529aae26041fb7c8efd77
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-16 09:19:26 -0400

    received and then added twitter keys

--- keys.js
+++ keys.js
@@ -1,8 +1,8 @@
 console.log('this is loaded');
 
 exports.twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '<07fpftCPssJfcMfQcmNEuUFKz>',
+  consumer_secret: '<w7IL26q2yPjCRoReKn7trwte59uedc2UoRkIhwUFCpnNu8jZTU>',
+  access_token_key: '<875701570977103873-CVDtpOCzmFuDNlq2ycuqkvlWmCsF4S7>',
+  access_token_secret: '<BUCb37KAuczBje4fsQlMRVILD6YLTgaF2wSFCUe6d0Uis>',
 }
