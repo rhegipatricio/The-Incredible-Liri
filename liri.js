478323ec0a4236921238ab693a62ea907f147de5
rhegipatricio <rhegi.patricio@gmail.com>
2017-06-16 09:35:33 -0400

    created vars on liri.js

--- liri.js
+++ liri.js
@@ -1,1 +1,19 @@
-var keys = require('./keys.js')
+
+var process2 = process.argv[2];
+var process3 = process.argv[3];
+
+var keys = require('./keys.js');
+var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
+var request = require('request');
+var fs = require ('fs'); 
+
+switch (process2){
+
+
+
+
+
+
+	
+}
